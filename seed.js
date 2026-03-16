1c5ad149d6b137c33eb1afee45f2a8a0ffb2abf8
tuancd258 <chuongvinhtuan2015@gmail.com>
2026-03-17 01:25:42 +0700

    a

--- seed.js
+++ seed.js
@@ -10,6 +10,7 @@ dotenv.config({ path: path.resolve(__dirname, './.env') });
 
 const dbConfig = {
   host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '',
   database: process.env.DB_NAME || 'ManagementInventory',
--- src/config/db.js
+++ src/config/db.js
@@ -6,6 +6,7 @@ dotenv.config();
 // Tạo kết nối pool để tối ưu hiệu năng
 const db = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '', 
   database: process.env.DB_NAME || 'ManagementInventory',
